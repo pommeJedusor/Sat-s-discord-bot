a2b76c5d9b34f463dfaed8bf48120a82a9cac360
Pomme Jedusor <lemonniergregory2@gmail.com>
2023-03-14 13:22:37 +0100

    ajout de la commande oubli_question

--- cogs/question.py
+++ cogs/question.py
@@ -1,7 +1,9 @@
 from discord import app_commands
 from discord.ext import commands
 import discord
+import datetime
 
+import global_functions
 import json, time
 
 from datas.datas import Datas
@@ -12,9 +14,33 @@ class Question(commands.Cog):
 
     @app_commands.command(name="question", description="à faire juste avant de poser la question de la semaine")
     async def question(self,interaction:discord.Interaction,gemmes:int):
-        with open(Datas.question_file,"w") as f:
-            f.write(json.dumps({"nb_gemmes": gemmes, "id_users": False, "starttime": time.time(), "message_id": interaction.user.id}))
-        await interaction.response.send_message(f"c'est fait",ephemeral=True)
+        if global_functions.bon_role(interaction.user):
+            with open(Datas.question_file,"w") as f:
+                f.write(json.dumps({"nb_gemmes": gemmes, "id_users": False, "starttime": time.time(), "message_id": interaction.user.id}))
+            await interaction.response.send_message(f"c'est fait",ephemeral=True)
+        else:
+            await interaction.response.send_message(f"c'vous n'avez pas le bon rôle",ephemeral=True)
+
+    @app_commands.command(name="oubli_question", description="si oublié d'utiliser la commande pour la question de la semaine")
+    async def oubli_question(self,interaction:discord.Interaction,gemmes:int,questionneur:discord.Member):
+        if global_functions.bon_role(interaction.user):
+            pomme=False
+            channel_question=self.bot.get_channel(Datas.channel_question)
+            async for message in channel_question.history(oldest_first=False):
+                if message.author.id==questionneur.id:
+                    with open(Datas.question_file,"w") as f:
+                        f.write(json.dumps({"nb_gemmes": gemmes, "id_users": [], "starttime": datetime.datetime.timestamp(message.created_at), "message_id": message.id}))
+                        pomme=True
+                        good_message=message
+                    break
+            if pomme:
+                await interaction.response.send_message(f"c'est fait",ephemeral=True)
+                async for message in channel_question.history(after=message,oldest_first=True):
+                    await self.bot.on_message(message)
+            else:
+                await interaction.response.send_message(f"message non trouvé",ephemeral=True)
+        else:
+            await interaction.response.send_message(f"vous n'avez pas le bon rôle",ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(Question(bot))
