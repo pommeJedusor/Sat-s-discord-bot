f8486408eee7cb9f9ef6170e6a9dcefb1c010341
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2023-08-17 21:34:42 +0200

    upgarde of the funs to support the delay and fix a bug for the func see_pity

--- cogs/powers.py
+++ cogs/powers.py
@@ -13,6 +13,7 @@ class Powers(commands.Cog):
 
     @app_commands.command(name="reset_power",description="permet de réactivé les effets des persos panda et arsmote")
     async def reset_power(self,interaction: discord.Interaction):
+        await interaction.response.defer()
         if global_functions.bon_role(interaction.user):
             text=""
             with open(Datas.player_file,"r") as f:
@@ -23,12 +24,13 @@ class Powers(commands.Cog):
                     text+=json.dumps(ligne)+"\n"
             with open(Datas.player_file,"w") as f:
                 f.write(text)
-            await interaction.response.send_message(":white_check_mark: **Le pouvoir a bien été réinitialisé.** :white_check_mark:")
+            await interaction.edit_original_response(content=":white_check_mark: **Le pouvoir a bien été réinitialisé.** :white_check_mark:")
         else:
-            await interaction.response.send_message("vous n'avez pas le bon role")
+            await interaction.edit_original_response(content="vous n'avez pas le bon role")
     
     @app_commands.command(name="yato",description=r"20% de chances d'obtenir 2 cristaux durant 10 tirages l'reste=items")
     async def yato(self,interaction: discord.Interaction):
+        await interaction.response.defer()
         player=global_functions.Player(interaction.user.name,interaction.user.id)
         player.is_player()
         if {"id":Datas.Yato_id,"active":True} in player.caracter[8] and not player.caracter[10]>0:
@@ -36,17 +38,18 @@ class Powers(commands.Cog):
             for power in player.caracter[8]:
                     if power['id']==Datas.Yato_id:
                         power['active']=False
-            await interaction.response.send_message("activé")
+            await interaction.edit_original_response(content="activé")
         elif  player.caracter[10]>0:
-            await interaction.response.send_message(f"il vous reste {player.caracter[10]} tirages où le pouvoir peut prendre effets")
+            await interaction.edit_original_response(content=f"il vous reste {player.caracter[10]} tirages où le pouvoir peut prendre effets")
         elif {"id":Datas.Yato_id,"active":False} in player.caracter[8]:
-            await interaction.response.send_message(f"votre pouvoir est désactivé")
+            await interaction.edit_original_response(content=f"votre pouvoir est désactivé")
         else:
-            await interaction.response.send_message(":x: **Vous n'avez pas l'objet requis pour effectuer cette commande.** :x:")
+            await interaction.edit_original_response(content=":x: **Vous n'avez pas l'objet requis pour effectuer cette commande.** :x:")
         player.update_stats_player_fichier()
     
     @app_commands.command(name="panda",description=r"utilise la capacité de panda et réduit de 10% la pity a avoir")
     async def panda(self,interaction: discord.Interaction):
+        await interaction.response.defer()
         player=global_functions.Player(interaction.user.name,interaction.user.id)
         player.is_player()
         if {"id":Datas.panda_id,"active":True} in player.caracter[8]:
@@ -72,11 +75,11 @@ class Powers(commands.Cog):
                 if power['id']==Datas.panda_id:
                     power['active']=False
             player.update_stats_player_fichier()
-            await interaction.response.send_message(f"votre pouvoir a bien été utilisé votre pity est désormais de : \n {player.caracter[6][0]}/{player.caracter[6][2]} pour les 4 étoiles \n {player.caracter[6][1]}/{player.caracter[6][3]} pour les 5 étoiles\n {player.caracter[6][4]}/{player.caracter[6][5]} pour les 6 étoiles")
+            await interaction.edit_original_response(content=f"votre pouvoir a bien été utilisé votre pity est désormais de : \n {player.caracter[6][0]}/{player.caracter[6][2]} pour les 4 étoiles \n {player.caracter[6][1]}/{player.caracter[6][3]} pour les 5 étoiles\n {player.caracter[6][4]}/{player.caracter[6][5]} pour les 6 étoiles")
         elif {"id":Datas.panda_id,"active":False} in player.caracter[8]:
-            await interaction.response.send_message("votre pouvoir est désactivé")
+            await interaction.edit_original_response(content="votre pouvoir est désactivé")
         else:
-            await interaction.response.send_message(":x: **Vous n'avez pas l'objet requis pour effectuer cette commande.** :x:")
+            await interaction.edit_original_response(content=":x: **Vous n'avez pas l'objet requis pour effectuer cette commande.** :x:")
 
 
 async def setup(bot):
--- cogs/tirage.py
+++ cogs/tirage.py
@@ -16,12 +16,13 @@ class Tirage(commands.Cog):
     @app_commands.command(name="tirage",description="permet de faire des tirages")
     async def tirages(self,interaction:discord.Interaction,nombre_de_tirage:int):
         global tirage_en_cours
+        await interaction.response.defer()
         player = global_functions.Player(interaction.user.id,interaction.user.id)
         player.is_player()
         channel_perso=self.bot.get_channel(player.caracter[5])
         if player.caracter[2]>=nombre_de_tirage and nombre_de_tirage<=10 and interaction.channel_id==Datas.channel_tirage_gacha and not tirage_en_cours:
             tirage_en_cours = True
-            await interaction.response.send_message(f"vous avez obtenus...")
+            await interaction.edit_original_response(content=f"vous avez obtenus...")
 
             #yato
             """if player.caracter[10]>0:
@@ -64,43 +65,47 @@ class Tirage(commands.Cog):
                     await interaction.channel.send("vous avez gagné 2 cristaux d'expedition")
             tirage_en_cours = False
         elif not interaction.channel_id==Datas.channel_tirage_gacha:
-            await interaction.response.send_message(f"vous êtes dans le mauvais channel aller dans tirage gacha pour effectuer cette commande",ephemeral=True)
+            await interaction.edit_original_response(content=f"vous êtes dans le mauvais channel aller dans tirage gacha pour effectuer cette commande")
         elif nombre_de_tirage>10:
-            await interaction.response.send_message("vous ne pouvez éffectuer qu'un maximum de 10 tirages à la fois",ephemeral=True)
+            await interaction.edit_original_response(content="vous ne pouvez éffectuer qu'un maximum de 10 tirages à la fois")
         elif tirage_en_cours:
-            await interaction.response.send_message(f"un tirage est déja en cours veuillez patienter et retenter plus tard",ephemeral=True)
+            await interaction.edit_original_response(content=f"un tirage est déja en cours veuillez patienter et retenter plus tard")
         else:
-            await interaction.response.send_message(f"il vous manque {nombre_de_tirage-player.caracter[2]} gemmes",ephemeral=True)
+            await interaction.edit_original_response(content=f"il vous manque {nombre_de_tirage-player.caracter[2]} gemmes")
 
     @app_commands.command(name="change_channel",description="permet de changer ou d'initialiser le channel d'un joueur")
     async def change_channel(self,interaction:discord.Interaction,user:discord.Member):
+        await interaction.response.defer()
         player = global_functions.Player(user.name,user.id)
         player.is_player()
         if global_functions.bon_role(interaction.user):
             player.caracter[5]=interaction.channel_id
             player.update_stats_player_fichier()
-            await interaction.response.send_message(f"{Datas.emogi_cristal}** Le Salon perso de __{user}__ est désormais __{interaction.channel.name}__. ** {Datas.emogi_cristal}")
+            await interaction.edit_original_response(content=f"{Datas.emogi_cristal}** Le Salon perso de __{user}__ est désormais __{interaction.channel.name}__. ** {Datas.emogi_cristal}")
         else:
-            await interaction.response.send_message("vous n'avez pas le bon role")
+            await interaction.edit_original_response(content="vous n'avez pas le bon role")
 
     @app_commands.command(name="see_pity",description="permet de voir sa pity")
     async def voir_sa_pity(self,interaction:discord.Interaction):
+        await interaction.response.defer(ephemeral=True)
         player=global_functions.Player(interaction.user.name,interaction.user.id)
         player.is_player()
-        await interaction.response.send_message(f"**Votre pity :star::star::star::star: : {player.caracter[6][0]}/{player.caracter[6][2]} **\n**Votre pity :star::star::star::star::star: : {player.caracter[6][1]}/{player.caracter[6][3]} **\n**Votre pity :star::star::star::star::star::star: : {player.caracter[6][4]}/{player.caracter[6][5]} **",ephemeral=True)
+        await interaction.edit_original_response(content=f"**Votre pity :star::star::star::star: : {player.caracter[6][0]}/{player.caracter[6][2]} **\n**Votre pity :star::star::star::star::star: : {player.caracter[6][1]}/{player.caracter[6][3]} **\n**Votre pity :star::star::star::star::star::star: : {player.caracter[6][4]}/{player.caracter[6][5]} **")
 
     @app_commands.command(name="see_pity_of_a_player",description="permet de voir la pity d'un joueur")
     async def see_pity_of_a_player(self,interaction:discord.Interaction,user:discord.Member):
+        await interaction.response.defer()
         if global_functions.bon_role(interaction.user):
             player=global_functions.Player(user.name,user.id)
             player.is_player()
-            await interaction.response.send_message(f"pour l'obtention d'une 4 étoiles: {player.caracter[6][0]}/{player.caracter[6][2]} \n pour l'obtention d'une 5 étoiles: {player.caracter[6][1]}/{player.caracter[6][3]}\n pour l'obtention d'une 6 étoiles: {player.caracter[6][4]}/{player.caracter[6][5]}")
+            await interaction.edit_original_response(content=f"pour l'obtention d'une 4 étoiles: {player.caracter[6][0]}/{player.caracter[6][2]} \n pour l'obtention d'une 5 étoiles: {player.caracter[6][1]}/{player.caracter[6][3]}\n pour l'obtention d'une 6 étoiles: {player.caracter[6][4]}/{player.caracter[6][5]}")
         else:
-            await interaction.response.send_message("vous n'avez pas le bon role")
+            await interaction.edit_original_response(content="vous n'avez pas le bon role")
 
     @app_commands.command(name="edit_pity_of_a_player",description="permet de modifier la pity d'un joueur")
-    async def see_pity_of_a_player(self,interaction:discord.Interaction,user:discord.Member,dénominateur_4:int=False,dénominateur_5:int=False,numérateur_4:int=False,numérateur_5:int=False,dénominateur_6:int=False,numérateur_6:int=False):
+    async def edit_pity_of_a_player(self,interaction:discord.Interaction,user:discord.Member,dénominateur_4:int=False,dénominateur_5:int=False,numérateur_4:int=False,numérateur_5:int=False,dénominateur_6:int=False,numérateur_6:int=False):
         if global_functions.bon_role(interaction.user):
+            await interaction.response.defer()
             player = global_functions.Player(user.name,user.id)
             player.is_player()
             if not numérateur_5:
@@ -117,9 +122,9 @@ class Tirage(commands.Cog):
                 dénominateur_6=player.caracter[6][5]
             player.caracter[6] = [numérateur_4,numérateur_5,dénominateur_4,dénominateur_5,numérateur_6,dénominateur_6]
             player.update_stats_player_fichier()
-            await interaction.response.send_message(f"le joueur {user.name} a désormais comme pity: \npour 4 étoiles: {player.caracter[6][0]}/{player .caracter[6][2]} \npour 5 étoiles {player.caracter[6][1]}/{player .caracter[6][3]} \net pour 6 étoiles: {player.caracter[6][4]}/{player .caracter[6][5]} ")
+            await interaction.edit_original_response(content=f"le joueur {user.name} a désormais comme pity: \npour 4 étoiles: {player.caracter[6][0]}/{player .caracter[6][2]} \npour 5 étoiles {player.caracter[6][1]}/{player .caracter[6][3]} \net pour 6 étoiles: {player.caracter[6][4]}/{player .caracter[6][5]} ")
         else:
-             await interaction.response.send_message("vous n'avez pas le bon role")   
+             await interaction.edit_original_response(content="vous n'avez pas le bon role")   
 
 async def setup(bot):
     await bot.add_cog(Tirage(bot))
