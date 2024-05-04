0be607c85ffe53ebf2f95a40eaa3ea60d0939ad6
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-05-04 18:13:45 +0200

    améloiration de l'ui de la commande arsmote 'menu défilant'

--- cogs/powers.py
+++ cogs/powers.py
@@ -42,39 +42,6 @@ class Powers(commands.Cog):
         else:
             print("le pouvoir est encore actif")
         player.update_stats_player_fichier()
-
-    @app_commands.command(name="arsmote",description="permet de reroll un 4 étoile obtnu lors du dernier tirage pour en obtenir un autre")
-    async def arsmote(self,interaction: discord.Interaction,nom_du_4_étoiles:str):
-        player=global_functions.Player(interaction.user.name,interaction.user.id)
-        player.is_player()
-        item=global_functions.Items(nom_du_4_étoiles)
-        channel_perso=self.bot.get_channel(player.caracter[5])
-        if {"id":Datas.arsmote_id,"active":True} in player.caracter[8] and item.is_item() and item.caracter[6] in player.caracter[9] and item.caracter[1]==4:
-            if player.lose_item(item.caracter[6],1):
-                text=""
-                for i in player.tirages(1,tirage_4=True):
-                    text+=f"{i.caracter[0]}: {i.caracter[1]} étoiles \n"
-                for power in player.caracter[8]:
-                    if power['id']==Datas.arsmote_id:
-                        power['active']=False
-                await interaction.response.send_message(text)
-                await channel_perso.send(text)
-                await interaction.channel.send(i.caracter[3])
-                await channel_perso.send(i.caracter[3])
-                player.update_stats_player_fichier()
-            else:
-                await interaction.response.send_message(f"l'item {item.caracter[0]} n'as pas réussis à être supprimé")
-        elif {"id":Datas.arsmote_id,"active":False} in player.caracter[8]:
-            await interaction.response.send_message("votre pouvoir est désactivé")
-        elif not {"id":Datas.arsmote_id,"active":True} in player.caracter[8]:
-            await interaction.response.send_message("vous n'avez pas l'item requis pour effectuer cette commande")
-        elif not item.is_item():
-            await interaction.response.send_message(f"l'item {nom_du_4_étoiles} n'as pas été trouvé")
-        elif not item.caracter[6] in player.caracter[9]:
-            await interaction.response.send_message(f"vous n'avez pas obtenu l'item {item.caracter[0]} lors de votre dernier tirage")
-        else:
-            await interaction.response.send_message(f"l'item {item.caracter[0]} n'est pas 4 étoiles mais {item.caracter[1]} étoiles ")
-
     
     @app_commands.command(name="panda",description=r"utilise la capacité de panda et réduit de 10% la pity a avoir")
     async def panda(self,interaction: discord.Interaction):
