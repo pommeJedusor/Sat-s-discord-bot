98ca7bd4d88bad2f416d58f10e8c57e4336f768c
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-05-04 18:13:44 +0200

    first commit

---
+++ datas/datas.py
@@ -0,0 +1,12 @@
+class Datas:
+    bot_token = "discord bot token"
+    panda_id="l'id de l'item"
+    arsmote_id="l'id de l'item"
+    Yato_id="l'id de l'item"
+    hosts_id="l'id de l'item"
+    role_modo=["l'id des roles modos","l'id des roles modos"]
+    channel_question="id du channel"
+    channel_message_bot = "id du channel"
+    question_file="datas/datas_question.txt"
+    player_file = "datas/datas_players.txt"
+    items_file = "datas/datas_items.txt"
